f1d7a0f7808b111217aed9c763f2bc71da08451f
franklin rodriguez <franklinserif@gmail.com>
2022-05-13 16:55:39 -0400

    basic planet model

--- src/models/planets.model.js
+++ src/models/planets.model.js
@@ -1,3 +1,36 @@
-const allPlanets = [];
+/* eslint-disable operator-linebreak */
+const { parse } = require('csv-parse');
+const fs = require('fs');
 
-module.exports = allPlanets;
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return (
+    planet?.koi_disposition === 'CONFIRMED' &&
+    planet.koi_insol > 0.36 &&
+    planet.koi_insol < 1.11 &&
+    planet.koi_prad < 1.6
+  );
+}
+
+fs.createReadStream('./src/kepler_data.csv')
+  .pipe(
+    parse({
+      comment: '#',
+      columns: true,
+    }),
+  )
+  .on('data', (data) => {
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
+  })
+  .on('error', (error) => {
+    console.log(error);
+  })
+  .on('end', () => {
+    console.log(habitablePlanets.map((planet) => planet.kepler_name));
+    console.log(`${habitablePlanets.length} habitable planets founds`);
+  });
+
+module.exports = habitablePlanets;
