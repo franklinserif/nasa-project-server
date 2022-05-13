091cc59bce9fca582bab114fe875ffe29470a98c
franklin rodriguez <franklinserif@gmail.com>
2022-05-13 05:19:15 -0400

    enviroment variable set on scripts

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 const http = require('http');
+const app = require('./app');
 
 const PORT = process.env.PORT || 8000;
+const ENVIROMENT = process.env.ENVIROMENT || 'dev';
 
-const server = http.createServer();
+const server = http.createServer(app);
 
 server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+  if (ENVIROMENT === 'dev') {
+    console.log(`Listening on port ${PORT}`);
+  }
 });
