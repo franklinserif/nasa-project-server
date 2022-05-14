deacd9cdddf4d289d0b9a584e105c1a63bdc2bda
franklin rodriguez <franklinserif@gmail.com>
2022-05-13 21:13:42 -0400

    new package.json script configuration and .gitinogre

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const planetRouter = require('./routes/planets/planets.router');
 
 const app = express();
@@ -13,6 +14,10 @@ app.use(
  * parse any incoming json request in req.body
  */
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetRouter);
+app.use('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public'));
+});
 
 module.exports = app;
