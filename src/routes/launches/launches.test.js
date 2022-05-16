7065053d531de925597564d13ee85dc879fc6cb1
franklin rodriguez <franklinserif@gmail.com>
2022-05-16 07:15:50 -0400

    launches test

---
+++ src/routes/launches/launches.test.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+describe('Test GET / launches', () => {
+  test('It should response with 200 success', () => {
+    const response = 200;
+    expect(response).toBe(200);
+  });
+});
+
+describe('Test POST / launches', () => {
+  test('It should response with 200 success', () => {});
+
+  test('It should catch missing required properties', () => {});
+
+  test('It should catch invalid dates', () => {});
+});
