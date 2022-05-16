716da813e5509212738c5f39255601b5ec81cbe0
franklin rodriguez <franklinserif@gmail.com>
2022-05-16 07:36:59 -0400

    test for post /launches endpoint

--- src/routes/launches/launches.test.js
+++ src/routes/launches/launches.test.js
@@ -1,13 +1,30 @@
 /* eslint-disable no-undef */
+const request = require('supertest');
+const app = require('../../app');
+
 describe('Test GET / launches', () => {
-  test('It should response with 200 success', () => {
-    const response = 200;
-    expect(response).toBe(200);
+  test('It should response with 200 success', async () => {
+    const response = await request(app)
+      .get('/launches')
+      .expect('Content-Type', /json/)
+      .expect(200);
   });
 });
 
 describe('Test POST / launches', () => {
-  test('It should response with 200 success', () => {});
+  test('It should response with 201 created', async () => {
+    const response = await (
+      await request(app).post('/launches')
+    )
+      .send({
+        mission: 'USS Enterprise',
+        rocket: 'NCC 1701-D',
+        target: 'Kepler-186 f',
+        launchDate: 'January 4, 2028',
+      })
+      .expect('Content-Type', /json/)
+      .expect(201);
+  });
 
   test('It should catch missing required properties', () => {});
 
