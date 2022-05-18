4cc914b322bcf379df7850e79a1a2e863ad18805
franklin rodriguez <franklinserif@gmail.com>
2022-05-17 21:20:05 -0400

    basic mongoose connection

--- src/server.js
+++ src/server.js
@@ -1,16 +1,30 @@
 const http = require('http');
+const mongoose = require('mongoose');
 const app = require('./app');
 const { loadPlanetsData } = require('./models/planets.model');
 
-const PORT = process.env.PORT || 4000;
+const { PORT } = process.env;
 const ENVIROMENT = process.env.ENVIROMENT || 'dev';
+const MONGO_URL = 'mongodb+srv://franklinserif:omYcf0PzCWE39tbk@nasacluster.xmw4a.mongodb.net/nasa?retryWrites=true&w=majority';
+
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (error) => {
+  console.error(error);
+});
 
 const server = http.createServer(app);
 
 /**
- * It will load data from csv and start the server.
+ * It will load data from csv and start the server
  */
 async function startServer() {
+  await mongoose.connect(MONGO_URL, {
+    useUnifiedTopology: true,
+  });
+
   await loadPlanetsData();
 
   server.listen(PORT, () => {
